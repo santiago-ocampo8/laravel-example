c35a707fd39cc2f43be6910ae30667bfb30bc0bb
santiago-ocampo8 <santiago.1701721006@ucaldas.edu.co>
2022-10-07 20:14:49 -0500

    Creation of migrations

---
+++ example-app/database/migrations/2022_10_08_010449_profiles.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Profiles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('phone_number');
+            $table->string('url_facebook');
+            $table->bigInteger('user_id')->unsigned()->unique();
+            //Hacer efectivo la relación
+            $table->foreign('user_id')->references('id')
+                                    ->on('users')
+                                    ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ example-app/database/migrations/2022_10_08_010505_permission_role.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PermissionRole extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->id();
+            //Agregar las columnas que contendrán los datos foráneos
+            $table->bigInteger('role_id')->unsigned();
+            $table->bigInteger('permission_id')->unsigned();
+            //Hacer efectivo la relación
+            $table->foreign('role_id')->references('id')
+                                    ->on('roles')
+                                    ->onDelete('cascade');
+
+            $table->foreign('permission_id')->references('id')
+                                    ->on('permissions')
+                                    ->onDelete('cascade');
+            $table->timestamps();
+            $table->unique(['role_id', 'permission_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
