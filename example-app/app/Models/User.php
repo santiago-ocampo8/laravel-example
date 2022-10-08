1d6eeeddfa8602e732acc91b578790c86cc148f8
santiago-ocampo8 <santiago.1701721006@ucaldas.edu.co>
2022-10-07 20:23:03 -0500

    Creation models

--- example-app/app/Models/User.php
+++ example-app/app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id'
     ];
 
     /**
@@ -41,4 +42,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    
 }
